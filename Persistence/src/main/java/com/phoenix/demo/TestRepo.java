d4cad0fc9be8ce4b45b46d76b11aad3135ae69b6
Phoenix <phoenix.firebird@gmail.com>
2021-07-13 11:06:35 +0530

    Added comments

--- Persistence/src/main/java/com/phoenix/demo/TestRepo.java
+++ Persistence/src/main/java/com/phoenix/demo/TestRepo.java
@@ -1,7 +1,13 @@
 package com.phoenix.demo;
 
 public class TestRepo {
+	/***
+	 * 
+	 * @param args
+	 */
   public static void main(String[] args)
   {
+	  //Class comments
+	  System.out.println("Testing code violations");
   }
 }
